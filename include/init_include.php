9f707454f1227503e796dbd2832b2775ed3fdc56
Tiền Minh Vy <tienminhvydev@gmail.com>
2020-08-30 16:11:45 +0700

    v1.0.9 Close Beta
    - Sửa lỗi
    - Cập nhật file settings.php

--- include/init_include.php
+++ include/init_include.php
@@ -3,7 +3,7 @@
         die('<h1>Truy cập trực tiếp bị cấm!</h1>');
     }
 
-    require_once('db.php');
+    require_once('settings.php');
 
     if (!defined('isInstalled')){
         die('<h1>Bạn cần chạy file caidat.php của eSEduVN trước khi sử dụng! (#00)</h1>');
@@ -15,7 +15,5 @@
 
     define("SITE_NAME", $db->getSingleData(DB_TABLE_PREFIX."caidat", "giatri", "tencaidat", "tenwebsite"));
 
-    date_default_timezone_set("Asia/Ho_Chi_Minh");
-
-    
+    date_default_timezone_set("Asia/Ho_Chi_Minh");    
 ?>
