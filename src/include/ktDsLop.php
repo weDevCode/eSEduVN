bc5252b9133436cc9878416d75d4ef0957c5d1ed
Tiền Minh Vy <tienminhvydev@gmail.com>
2020-08-30 14:34:34 +0700

    v1.0.5 Close Beta
    - Cập nhật tính năng báo lỗi khi quản trị viên chưa định nghĩa khối học buổi sáng hay chiều

--- src/include/ktDsLop.php
+++ src/include/ktDsLop.php
@@ -10,12 +10,19 @@
         for ($i=0; $i < count($dskhoi); $i++) { 
             
             $count = $db->getSingleData(DB_TABLE_PREFIX.'dslop', 'COUNT(*)', 'khoi', $dskhoi[$i]['khoi']);
-            
+
             if ($count==0) {
             
                 die('<h1>Quản trị viên eSEduVN cần nhập danh sách lớp và khối mới có thể xem trang này (#02)</h1>');
             
             }
+        }
+
+        $count2 = $db->getSingleData(DB_TABLE_PREFIX.'quydinh', 'COUNT(*)', 'buoi', 'a');
+        if ($count2>0) {
+
+            die('<h1>Quản trị viên eSEduVN cần xác định buổi học của toàn bộ khối. Ví dụ khối nào học buổi sáng, khối nào học buổi chiều (#02.1)</h1>');
+
         }
     }
 ?>
