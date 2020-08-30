ad1294a5c46dc13cc3185d807d3e6d38df631891
Tiền Minh Vy <tienminhvydev@gmail.com>
2020-08-30 14:40:10 +0700

    v1.0.6 Close Beta
    Sửa lỗi báo lỗi sai trong trang admin

--- src/quantri/caidatchung.php
+++ src/quantri/caidatchung.php
@@ -193,7 +193,47 @@
 
                 $thoiluong = $db->getSingleData(DB_TABLE_PREFIX.'caidat', 'giatri', 'tencaidat', 'thoiluongtiet');
                 
-                require_once("../include/ktgiovaotiet.php");
+                $giovaotietdb = $db->getMulData(DB_TABLE_PREFIX.'giovaotiet', array(
+                    'ten',
+                    'thoiluong'
+                ));
+                if ($giovaotietdb==0) {
+                    // chưa xử lý
+                } elseif (count($giovaotietdb)==20) {
+
+                    for ($i=0; $i < count($giovaotietdb); $i++) { 
+            
+                        $ten1 = ''; $ten2 = '';
+            
+                        foreach ($giovaotietdb[$i] as $key => $value) {
+            
+                            if ($key == 'ten') {
+            
+                                $ten1 = $value;
+            
+                            } elseif ($key == 'thoiluong') {
+            
+                                $ten2 = $value;
+            
+                            }
+            
+                            $giovaotiet[$ten1] = $ten2;
+            
+                        }
+                    }
+            
+                    for ($i=1; $i < 6; $i++) {
+            
+                        $sang["$i-gio"] = $giovaotiet["sang-$i-gio"];
+            
+                        $sang["$i-phut"] = $giovaotiet["sang-$i-phut"];
+            
+                        $chieu["$i-gio"] = $giovaotiet["chieu-$i-gio"];
+            
+                        $chieu["$i-phut"] = $giovaotiet["chieu-$i-phut"];
+                        
+                    }
+                }
 
                 $content = <<<HTML
                     <h3 class="text-center">Giờ vào tiết</h3>
